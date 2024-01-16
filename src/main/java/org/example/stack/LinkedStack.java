7a8db19d49f7a32b70ed77408d290d2e565e15f6
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-16 06:35:34 +0000

    added boilerplate for singly linked list based stack

---
+++ src/main/java/org/example/stack/LinkedStack.java
@@ -0,0 +1,29 @@
+package org.example.stack;
+
+public class LinkedStack<E> implements Stack<E> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E top() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+}
